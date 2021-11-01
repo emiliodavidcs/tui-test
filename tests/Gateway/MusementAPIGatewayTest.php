2f3dad5396d1b93867e89d1d63159fc533951e17
Emilio David Clemente Silmi <emiliodavidcs@gmail.com>
2021-11-01 16:44:18 +0100

    Create tests for musement gateway

---
+++ tests/Gateway/MusementAPIGatewayTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Emilioclemente\TuiApp\Tests\Gateway;
+
+use Emilioclemente\TuiApp\Gateway\HttpClient;
+use Emilioclemente\TuiApp\Gateway\MusementAPIGateway;
+use PHPUnit\Framework\TestCase;
+
+class MusementAPIGatewayTest extends TestCase
+{
+    public function testGetCities(){
+        // arrange
+        $mockCity = ['name' => 'Mock City'];
+        $httpClient = self::createMock(HttpClient::class);
+        $httpClient->expects(self::once())->method('get')->willReturn([$mockCity]);
+        $gateway = new MusementAPIGateway($httpClient);
+
+        // act
+        $res = $gateway->getCities();
+
+        // assert
+        self::assertEquals($mockCity['name'], $res[0]['name']);
+    }
+}
