0890fff3eb3816182e4ef3f18b41b07e57067638
Emilio David Clemente Silmi <emiliodavidcs@gmail.com>
2021-11-01 15:50:50 +0100

    Add constants that contain third party APIs static information

---
+++ src/Constants/MusementAPIConstants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Emilioclemente\TuiApp\Constants;
+
+class MusementAPIConstants
+{
+    const BASE_URL = 'https://api.musement.com';
+    const ENDPOINT_CITIES = '/api/v3/cities.json';
+}
---
+++ src/Constants/WeatherAPIConstants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Emilioclemente\TuiApp\Constants;
+
+class WeatherAPIConstants
+{
+    const BASE_URL = 'http://api.weatherapi.com';
+    const ENDPOINT_FORECAST = '/v1/forecast.json';
+}
